20ab96bbe4244d15a798f18b1fd5da5cbdaab998
gaudelbijay <gaudelbijay10@gmail.com>
2021-03-01 16:27:32 +0545

    rcv response from drone

--- drone_manager.py
+++ drone_manager.py
@@ -1,6 +1,7 @@
 import logging
 import socket
 import sys
+import threading
 import time
 
 logging.basicConfig(level=logging.INFO, steam=sys.stdout)
@@ -16,19 +17,58 @@ class DroneManager(object):
         self.drone_address = (drone_ip, drone_port)
         self.socket = socket.socket(socket.socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind(self.host_ip, self.host_port)
-        self.socket.sendto(b'command', self.drone_address)  # To activate tello sdk
-        self.socket.sendto(b'streamon', self.drone_address)
+        self.send_command(b'command', self.drone_address)  # To activate tello sdk
+        self.send_command(b'streamon', self.drone_address)
+
+        self.response = None
+        self.stop_event = threading.Event()
+
+        self._response_thread = threading.Thread(target=self.receive_response, args=(self.stop_event,))
+        self._response_thread.start()
+
+    def receive_response(self, stop_event):
+        while not stop_event.is_set():
+            try:
+                self.response, ip = self.socket.recvfrom(1024)
+                logger.info({'action': 'receive_respone', 'response': self.response})
+            except socket.error as ex:
+                logger.error({'action': 'recieve_response', 'ex': ex})
+                break
 
     def __dell__(self, ):
         self.stop()
 
-    def stop(self, ):
+    def stop(self, retry):
+        self.stop_event.set()
+
+        while self._response_thread.isAlive():
+            time.sleep(0.3)
+            if retry > 30:
+                break
+            retry += 1
+
         self.socket.close()
 
-    def send_command(self, command):
+    def send_command(self, command, retry=0):
         logger.info({'action': 'send_command', 'command': command})
 
         self.socket.sendto(command.encode('utf-8'), self.drone_address)
+
+        while self.response is None:
+            time.sleep(0.3)
+
+            if retry > 5:
+                break
+            retry += 1
+
+        if self.response is None:
+            response = None
+        else:
+            response = self.response.decode('utf-8')
+
+        self.response = None
+        
+        return response
 
     def takeoff(self, ):
         self.send_command('takeoff')
